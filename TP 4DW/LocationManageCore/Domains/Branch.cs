ebcfd038dacd374c6649bed7148ab86b6aba5562
Samuel Kple-Faget <2377816@etu.cegepsth.qc.ca>
2026-02-23 13:33:32 -0500

    Ajout des dossiers Data et Domains Ajout des domaines Brand,car,location,Driver,Address,User

---
+++ TP 4DW/LocationManageCore/Domains/Branch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocationManageCore.Domains
+{
+    public class Branch
+    {
+        //Propriétés métiers 
+        public Guid Id { get; private set; }
+        public required bool Status { get; set; }
+        public required string Name { get; set; }
+
+        public static Branch Create(
+            bool status,
+            string name)
+        {
+            return new Branch
+            {
+                Id = Guid.NewGuid(),
+                Status = status,
+                Name = name
+            };
+    }
+}
---
+++ TP 4DW/LocationManageCore/Domains/Driver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocationManageCore.Domains
+{
+    public class Driver
+    {
+        public Guid Id { get; set; }
+
+        public required string LastName { get; set; }
+
+        public required string FirstName { get; set; }
+
+        public required string EmailAdress { get; set; }
+
+        public required string PhoneNumber { get; set; }
+         
+        public required string DriverLicenceNumber { get; set; } 
+
+        public static Driver Create(
+            string firstname,
+            string lastname,
+            string emailadress,
+            string phonenumber,
+            string driverlicensenumber)
+        {
+            return new Driver
+            {
+                Id = Guid.NewGuid(),
+                FirstName = firstname,
+                LastName = lastname,
+                EmailAdress = emailadress,
+                PhoneNumber = phonenumber,
+                DriverLicenceNumber = driverlicensenumber
+            };
+        }
+
+    }
+}
