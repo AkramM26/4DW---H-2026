392fffb2eca34b4fa6590e3019ccbbf422a4cc84
Gaby <gabrieldurette17@gmail.com>
2026-02-26 20:06:47 -0500

    Création d'applicationUser et de Branch

---
+++ TP 4DW/TP1/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TP1.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Lien avec la succursale
+        public int? BranchId { get; set; }
+        public virtual Branch Branch { get; set; }
+    }
+}
---
+++ TP 4DW/TP1/Models/Branch.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TP1.Models
+{
+    public class Branch
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        // Propriétés de navigation
+        public virtual ICollection<Car> Cars { get; set; }
+    }
+}
