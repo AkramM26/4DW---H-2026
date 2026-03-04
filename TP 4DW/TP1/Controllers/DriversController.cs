86798edac0a61d08d4ade2a9fc0994a1174b551d
GabrielB-38 <gabriel.bedard27@gmail.com>
2026-03-04 15:50:43 -0500

    ajout du LocationsController et des vues Index et Details (UC15, UC16)

---
+++ TP 4DW/TP1/Controllers/DriversController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP1.Controllers
+{
+    public class DriversController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TP 4DW/TP1/Controllers/LocationsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TP1.Data;
+
+namespace TP1.Controllers
+{
+    public class LocationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        // Le constructeur reçoit la DB
+        public LocationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allLocations = await _context.Locations
+                .Include(l => l.Car)
+                .Include(l => l.Driver)
+                .ToListAsync();
+
+            // UC15, séparation des loc. courantes et passées
+            ViewBag.Courantes = allLocations.Where(l => l.OfficialClosing == null).ToList();
+            ViewBag.Passees = allLocations.Where(l => l.OfficialClosing != null).ToList();
+            return View();
+        }
+
+        // UC16, consulter une location
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var location = await _context.Locations
+                .Include(l => l.Car)
+                .Include(l => l.Driver)
+                    .ThenInclude(d => d.Address) 
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return View(location);
+        }
+    }
+}
