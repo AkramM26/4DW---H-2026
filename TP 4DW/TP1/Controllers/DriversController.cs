57dc69e4a02da27cdf1641a1764f2f2e7a2b2de7
GabrielB-38 <gabriel.bedard27@gmail.com>
2026-03-08 17:42:36 -0400

    Ajout des actions Details et Index dans DriversController et ajout des vues Details et index pour les drivers

--- TP 4DW/TP1/Controllers/DriversController.cs
+++ TP 4DW/TP1/Controllers/DriversController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using LocationManageCore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 namespace TP1.Controllers
 {
     public class DriversController : Controller
     {
-        public IActionResult Index()
+        // Réference à DbContext
+        private readonly ApplicationDbContext _context;
+
+        public DriversController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var drivers = await _context.Drivers
+                .Include(d => d.Locations)
+                    .ThenInclude(l => l.Car)
+                .ToListAsync();
+
+            return View(drivers);
+        }
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var driver = await _context.Drivers
+                .Include(d => d.Locations)
+                    .ThenInclude(l => l.Car)
+                .FirstOrDefaultAsync(d => d.Id == id);
+            if (driver == null)
+            {
+                return NotFound();
+            }
+            return View(driver);
         }
     }
 }
