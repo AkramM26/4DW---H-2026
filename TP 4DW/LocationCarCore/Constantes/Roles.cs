a69ae1f506e117e408521429ec462cab8e476c57
Samuel Kple-Faget <2377816@etu.cegepsth.qc.ca>
2026-04-17 15:25:21 -0400

    Résolutions d'erreurs

---
+++ TP 4DW/LocationCarCore/Constantes/Roles.cs
@@ -0,0 +1,11 @@
+﻿namespace TP1.Constantes
+{
+    public class Roles
+    {
+        public const string ADMIN = "Admin";
+        public const string MANAGER = "Manager";
+        public const string CLERK = "Clerk";
+        public const string USER = "User";
+    }
+
+}
